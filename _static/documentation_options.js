c78d3dccf187d16c91d4d60cf7399ea9f987d66d
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-10-23 15:06:12 +0000

    Update docs after building Travis build 33 of Fratorhe/micropyro
    The docs were built from the branch 'master' against the commit
    ed1fbcd4542aec4448826b2872fc8ecce7685ba7.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/Fratorhe/micropyro/jobs/738324151.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.7/bin/doctr deploy --built-docs docs/build/html .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.post11+gc80e0b4',
+    VERSION: '0.1.post13+ged1fbcd',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
